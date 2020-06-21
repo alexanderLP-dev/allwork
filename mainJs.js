e00a8dee0287844242f14fc440ea9c5061a673b8
alexanderLP-dev <golovninalexandr87@gmail.com>
2020-06-21 21:20:35 +0300

    all

--- mainJs.js
+++ mainJs.js
@@ -25,8 +25,11 @@ btn[0].addEventListener("click", function () {
   }
 });
 
-btn[1].addEventListener("click", function () {
+btn[1].addEventListener("click", function (event) {
+  event.preventDefault();
   moveSlide();
+  // btn[1].style.transform = "translateX(5px)";
+
   if (currentSlide == items.length - 1) {
     currentSlide = 0;
   } else {
@@ -35,17 +38,49 @@ btn[1].addEventListener("click", function () {
 });
 function moveNext() {}
 
-// ===========================
+let moveBtn = function () {
+  btn[1].style.transform = "translateX(10px)";
+  btn[1].style.transition = "1s";
+  btn[0].style.transform = "translateX(-10px)";
+  btn[0].style.transition = "1s";
+};
+let backBtn = function () {
+  btn[0].style.transform = "translateX(10px)";
 
-let footerContact = document.querySelector(
-  ".main_block_footer > .block_contact"
-);
-let headerContact = document.querySelector(".block_contact");
+  btn[1].style.transform = "translateX(-10px)";
+};
 
-if (innerWidth < 1024) {
-  headerContact.style.display = "none";
-  footerContact.style.display = "block";
-} else {
-  headerContact.style.display = "block";
-  footerContact.style.display = "none";
+let repeate = function () {
+  setTimeout(moveBtn, 1000);
+  setTimeout(backBtn, 1200);
+};
+
+let intervalId2 = setInterval(repeate, 5000);
+
+let skillsIcons = document.querySelectorAll(".skills");
+let skillsItem = document.querySelectorAll(".skills-item");
+
+for (
+  let i = 0, k = 0;
+  i < skillsIcons.length, k < skillsItem.length;
+  i++, k++
+) {
+  if (i > 0) {
+    skillsItem[i].classList.add("hideSkill");
+  }
+
+  skillsIcons[i].onclick = function () {
+    skillsItem[i].classList.remove("hideSkill");
+
+    if ([i] > 0) {
+      skillsItem[i - 1].classList.remove("activeSkill");
+      skillsItem[i - 1].classList.add("hideSkill");
+      skillsItem[0].classList.add("hideSkill");
+    }
+    if ([i] < skillsItem.length - 1) {
+      skillsItem[i + 1].classList.remove("activeSkill");
+      skillsItem[i + 1].classList.add("hideSkill");
+      skillsItem[skillsItem.length - 1].classList.add("hideSkill");
+    }
+  };
 }
